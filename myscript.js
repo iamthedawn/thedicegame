c43501c326114311cf0d619f99162410b4221018
Nishant Mourya <2019kuec2010@iiitkota.ac.in>
2021-06-18 02:18:28 +0530

    added files of proj

---
+++ myscript.js
@@ -0,0 +1,56 @@
+var n=0,m=0;
+n = Math.random();
+n=n*6+1;
+n=Math.floor(n);
+m = Math.random();
+m=m*6+1;
+m=Math.floor(m);
+
+if(n===1){
+    document.querySelector(".img1").setAttribute("src","images/dice1.png");
+}
+else if(n===2){
+    document.querySelector(".img1").setAttribute("src","images/dice2.png");
+}
+else if(n===3){
+    document.querySelector(".img1").setAttribute("src","images/dice3.png");
+}
+else if(n===4){
+    document.querySelector(".img1").setAttribute("src","images/dice4.png");
+}
+else if(n===5){
+    document.querySelector(".img1").setAttribute("src","images/dice5.png");
+}
+else {
+    document.querySelector(".img1").setAttribute("src","images/dice6.png");
+}
+
+
+if(m===1){
+    document.querySelector(".img2").setAttribute("src","images/dice1.png");
+}
+else if(m===2){
+    document.querySelector(".img2").setAttribute("src","images/dice2.png");
+}
+else if(m===3){
+    document.querySelector(".img2").setAttribute("src","images/dice3.png");
+}
+else if(m===4){
+    document.querySelector(".img2").setAttribute("src","images/dice4.png");
+}
+else if(m===5){
+    document.querySelector(".img2").setAttribute("src","images/dice5.png");
+}
+else {
+    document.querySelector(".img2").setAttribute("src","images/dice6.png");
+}
+
+if(n===m && n!=0 && m!=0){
+    document.querySelector(".ref").innerHTML="It's a Draw!!";
+}
+if(n>m){
+    document.querySelector(".ref").innerHTML="Player1 Wins!!";
+}
+if(n<m){
+    document.querySelector(".ref").innerHTML="Player2 Wins!!";
+}
